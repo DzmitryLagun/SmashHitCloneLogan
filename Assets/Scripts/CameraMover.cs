ffa39ad34ca2b64a3385ed5b643a889cd4cf4b22
Logan <nope@nope.nope>
2019-12-11 21:28:20 +0300

    gitignor

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+
+    private void Update()
+    {
+
+        float speed = 2.0f;
+
+        Vector3 translateForward = Vector3.forward * speed * Time.deltaTime;
+
+        transform.Translate(translateForward, Space.World);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.Rotate(2.0f, 0.0f, 0.0f);
+    }
+}
