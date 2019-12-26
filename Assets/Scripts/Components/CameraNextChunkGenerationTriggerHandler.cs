0b5233aa6da51e9970248387529177dec9e3c9d5
Logan <nope@nope.nope>
2019-12-26 21:27:44 +0300

    SpawnpointsnewChunks and triggers

---
+++ Assets/Scripts/Components/CameraNextChunkGenerationTriggerHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraNextChunkGenerationTriggerHandler : MonoBehaviour
+{
+    public Action EventTriggerEntered { get; set; }
+    private void OnTriggerEnter(Collider other)
+    {
+        EventTriggerEntered?.Invoke();
+
+    }
+}
--- Assets/Scripts/Managers/GameplayManager.cs
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -3,15 +3,45 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    [SerializeField]
+    private CameraNextChunkGenerationTriggerHandler _cameraTriggerHandler = null;
+
+    private Chunk _currentChunk = null;
     private void Start()
+    {
+        SpawnNextChunk();
+    }
+
+    private void OnEnable()
+    {
+        _cameraTriggerHandler.EventTriggerEntered += OnCameraEnteredNextChunkGenerationTrigger;
+    }
+
+    private void OnDisable()
+    {
+        _cameraTriggerHandler.EventTriggerEntered -= OnCameraEnteredNextChunkGenerationTrigger;
+    }
+
+    private void SpawnNextChunk()
     {
         var settingsManager = SettingsManager.Instance;
         int index = Random.Range(0, settingsManager.ChunkPrefabs.Count);
         var chunkPrefab = settingsManager.ChunkPrefabs[index];
         var newChunk = Instantiate(chunkPrefab);
-    }
 
-    private void Update()
+        if (_currentChunk != null)
+        {
+            newChunk.transform.position = _currentChunk.NextChunkSpawnPoint.position;
+            newChunk.transform.rotation = _currentChunk.NextChunkSpawnPoint.rotation;
+        }
+        else
+        {
+            newChunk.transform.position = Vector3.zero;
+            newChunk.transform.rotation = Quaternion.identity;
+        }
+        _currentChunk = newChunk;
+    }
+    private void OnCameraEnteredNextChunkGenerationTrigger()
     {
 
     }
